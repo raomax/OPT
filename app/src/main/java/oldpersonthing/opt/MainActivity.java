5523e2571aeda618e4092d0aac633dc84676ae65
raomax <raomax@gmail.com>
2015-03-28 00:41:42 -0400

    Wrote Data to the database

--- app/src/main/java/oldpersonthing/opt/MainActivity.java
+++ app/src/main/java/oldpersonthing/opt/MainActivity.java
@@ -5,14 +5,31 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.Firebase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MainActivity extends ActionBarActivity {
-
+    Firebase ref;
+    Firebase usersRef;
+    NurseUser userTest1;
+    Map<String, NurseUser> users;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
 
+        ref = new Firebase("https://dazzling-heat-1446.firebaseio.com/");
+        usersRef = ref.child("users");
+        userTest1 = new NurseUser("userTest1");
+        users = new HashMap<String, NurseUser>();
+
+        users.put("userTest1",userTest1);
+
+        usersRef.setValue(users);
     }
 
 
@@ -20,6 +37,8 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        
         return true;
     }
 
--- app/src/main/java/oldpersonthing/opt/NurseUser.java
+++ app/src/main/java/oldpersonthing/opt/NurseUser.java
@@ -4,4 +4,17 @@ package oldpersonthing.opt;
  * Created by Robin Onsay on 3/27/2015.
  */
 public class NurseUser {
+    private String userName;
+
+    public NurseUser(){
+
+    }
+
+    public NurseUser(String userName){
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
 }
